435d5adee1ca3f227b23f9883c115366da4fd1ed
XiaoYeZi <qaz6750@outlook.com>
2024-08-12 20:49:30 +0800

    FTS521: Separate setScanMode
    Signed-off-by: XiaoYeZi <qaz6750@outlook.com>

---
+++ src/fts521/ftscore.c
@@ -0,0 +1,42 @@
+/*++
+      Module Name:
+
+            ftscore.c
+
+      Abstract:
+
+            Contains FingerTipS521 Core code
+
+      Environment:
+
+            Kernel mode
+
+      Revision History:
+
+--*/
+
+#include <Cross Platform Shim\compat.h>
+#include <fts521\ftsinternal.h>
+#include <ftscore.tmh>
+
+NTSTATUS 
+SetScanMode(
+    WDFDEVICE Device,
+    SPB_CONTEXT* SpbContext,
+    BYTE Mode,
+    BYTE Settings)
+{
+    NTSTATUS status;
+    BYTE FTS521_SCAN_MODE[3] = { FTS_CMD_SCAN_MODE, Mode, Settings };
+
+    status = FtsWrite(SpbContext, FTS521_SCAN_MODE, 3);
+    if (NT_SUCCESS(status))
+    {
+        Trace(
+            TRACE_LEVEL_ERROR,
+            TRACE_INTERRUPT,
+            "Setting scan mode Error.");
+    }
+
+    return STATUS_SUCCESS;
+}

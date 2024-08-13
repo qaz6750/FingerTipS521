11e3063d3a1a1eaed78b28c5e6942e28529a5d07
XiaoYeZi <qaz6750@outlook.com>
2024-08-13 13:41:32 +0800

    FTS521: Correct configuration of FTS521
    Signed-off-by: XiaoYeZi <qaz6750@outlook.com>

--- Include/fts521/ftsinternal.h
+++ Include/fts521/ftsinternal.h
@@ -68,6 +68,25 @@
 // Ignore warning C4324: 'xxx' : structure was padded due to __declspec(align())
 #pragma warning (disable : 4324)
 
+#ifndef RELATIVE
+#define RELATIVE(wait) (-(wait))
+#endif
+
+#ifndef NANOSECONDS
+#define NANOSECONDS(nanos) \
+	(((signed __int64)(nanos)) / 100L)
+#endif
+
+#ifndef MICROSECONDS
+#define MICROSECONDS(micros) \
+	(((signed __int64)(micros)) * NANOSECONDS(1000L))
+#endif
+
+#ifndef MILLISECONDS
+#define MILLISECONDS(milli) \
+	(((signed __int64)(milli)) * MICROSECONDS(1000L))
+#endif
+
 typedef struct _FOCAL_TECH_TOUCH_DATA
 {
     BYTE PositionX_High : 4;
